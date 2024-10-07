4b1f95350f35e9fbc1628d2ada9c6662fb09581b
Jamie Thompson <bishbashboshjt@gmail.com>
2024-10-07 12:14:25 +0200

    ensure toc highlights at least one element

--- _docs/static/js/nav-toc.js
+++ _docs/static/js/nav-toc.js
@@ -2,23 +2,27 @@
 window.addEventListener("DOMContentLoaded", () => {
   const shouldScroll = () => (window.innerWidth >= 992 ? true : false);
 
+  const activeIds = new Set();
+  const exited = new Set();
+
+  const select = (id) => {
+    const selector = `nav li a[href="#${id}"]`;
+    return document.querySelector(selector);
+  };
+
   const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
       const id = entry.target.getAttribute("id");
-
-      const selector = `nav li a[href="#${id}"]`;
-      const link = document.querySelector(selector);
+      const link = select(id);
       link.addEventListener("click", function (e) {
         e.preventDefault();
-        const elem = document.querySelector(`[id="${id}"]`);
-        if (elem) {
-          elem.scrollIntoView({ behavior: "smooth", block: "start" });
-        }
+        entry.target.scrollIntoView({ behavior: "smooth", block: "start" });
       });
 
       const parent = link.parentElement;
       if (entry.intersectionRatio > 0) {
         parent.classList.add("active");
+        activeIds.add(id);
         if (shouldScroll()) {
           parent.scrollIntoView({
             behavior: "smooth",
@@ -27,14 +31,25 @@ window.addEventListener("DOMContentLoaded", () => {
           });
         }
       } else {
-        parent.classList.remove("active");
+        exited.add(id);
       }
     });
+
+    const willRemain = [...activeIds].filter((x) => !exited.has(x));
+    if (willRemain.length > 0) {
+      // keep at least one active, even if nothing is intersecting
+      exited.forEach((id) => {
+        activeIds.delete(id);
+        const link = select(id);
+        const parent = link.parentElement;
+        parent.classList.remove("active");
+      });
+      exited.clear();
+    }
   });
 
   // Track all sections that have an `id` applied
   document.querySelectorAll(".anchor-link__source[id]").forEach((section) => {
-    console.log("section: ", section);
     observer.observe(section);
   });
 });
